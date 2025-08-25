b0787bd93288d607b9374669725008b37a9659c2
brian lopez <brianlama1220@gmail.com>
2025-08-25 18:16:41 -0400

    feat: increase max-width for improved layout in App.css and Consulta component

--- src/pages/Consulta.tsx
+++ src/pages/Consulta.tsx
@@ -2,7 +2,7 @@ import QueryInput from '../components/QueryInput';
 
 export default function Consulta() {
   return (
-    <main className="container mx-auto px-2 sm:px-4 lg:px-6 py-12">
+    <main className="max-w-[1800px] mx-auto px-2 sm:px-4 lg:px-6 py-12">
       <h1 className="text-4xl font-bold text-center text-gray-800 mb-12">Consulta de Comprobante Fiscal</h1>
       <QueryInput />
     </main>
